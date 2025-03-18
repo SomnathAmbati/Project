0375cd3e59a5a5006eaaba010ad891e069967e38
SomnathAmbati <somnath.ambati@gmail.com>
2025-03-18 18:50:15 +0530

    f

--- keys.js
+++ keys.js
@@ -2,5 +2,5 @@ require("dotenv").config(); // Load environment variables from .env file
 
 module.exports = {
     MONGO_URI: process.env.MONGO_URI,  // Fetch from environment variable
-    JWT_SECRET: process.env.JWT_SECRET // Fetch from environment variable
+    JWT_SECRET: process.env.JWT_SECRET, // Fetch from environment variable
 };
